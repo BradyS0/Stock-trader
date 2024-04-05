305d41a3af1a9a96ddcf1241065539b80ad36c6f
BradyS0 <starwarsboi77@gmail.com>
2024-04-05 02:05:35 -0500

    remove code, file is placeholder for future plans

--- api_funtions.py
+++ api_funtions.py
@@ -1,17 +1,1 @@
-import json
-
-def getAPIKey():
-    with open("config.json", r) as f:
-        config = json.loads(f)
-    return config
-
-api_key = getAPIKey()
-ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-def set_stock_price(stockID, ticker,today):
-    endpoint = f'price?symbol={ticker}&apikey={api_key}'
-    price = make_api_request(endpoint)["price"]
-    updateDatabase("UPDATE Stock SET lastModified = ? WHERE stockID = ?", (today,stockID))
-    updateDatabase("INSERT INTO Prices (date, stockID, price) VALUES (?,?,?)", (today,stockID,price))
-    return price
-
+#plan to move all api funtions into here
